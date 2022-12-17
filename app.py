37695243cf0d613b52db7ec338fefcf2e05bdb43
TomasMiqueleiz02 <108294311+TomasMiqueleiz02@users.noreply.github.com>
2022-12-17 16:47:54 -0300

    Commit 17/12

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, Response, request
 import json
 from os import system
+from http import HTTPStatus
 
 app = Flask(__name__)
 if __name__ == "__main__":
     app.run(debug=True)
 
+
 with open('peliculas.json', encoding='utf-8') as archivo_json:
     peliculas = json.load(archivo_json)
+
 
 with open('usuarios.json', encoding='utf-8') as archivo2_json:
     usuarios = json.load(archivo2_json)
 
-modo = True #Si modo es false es publico, si es true es privado
+
+modo = False #Si modo es false es publico, si es true es privado
 
 
 #Ultimas 10 peliculas modo publico
@@ -26,9 +30,13 @@ def nolog():
             continue
     return listapeliculas
 
+
+
 #Modo logueado
 @app.route("/usuarios/<usuario>/<contrasenia>", methods=["GET"])
 def registro(usuario, contrasenia):
+    global modo
+    modo = False
     for nombre in usuarios:
         if usuario == nombre["usuario"]:
             id_nombre = nombre["id"]
@@ -37,7 +45,12 @@ def registro(usuario, contrasenia):
                     id_contraseña = clave["id"]
                     if id_nombre == id_contraseña:
                         modo = True
-                        return "<h1>Logueado</h1>"                                           
+    if modo == True:
+        return Response(status=HTTPStatus.OK)
+    else:
+        return "<h1>Usuario/Contraseña no encontrados</h1>"                                          
+
+
 
 #Devolver la lista de directores presentes en la plataforma
 @app.route("/alldirectores", methods=["GET"])   
@@ -46,7 +59,11 @@ def alldirectores():
         listadirectores = []
         for pelicula in peliculas:
             listadirectores.append(pelicula["director"])
-    return listadirectores
+        return listadirectores
+    else:
+        return "<h1> No estas logueado</h1>"
+
+
 
 #Devolver la lista de películas dirigidas por un director en particular
 @app.route("/directores/<director>")
@@ -56,7 +73,11 @@ def directores(director):
         for pelicula in peliculas:
             if director == pelicula["director"]:
                 director_peliculas.append(pelicula["title"])
-    return director_peliculas
+        return director_peliculas
+    else:
+        return "<h1> No estas logueado</h1>"
+
+
 
 #Devolver la lista de géneros presentes en la plataforma
 @app.route("/generos", methods=["GET"])
@@ -65,96 +86,134 @@ def generos():
         listageneros = []
         for pelicula in peliculas:
             listageneros.append(pelicula["genero"])
-    return listageneros
+        return listageneros
+    else:
+        return "<h1> No estas logueado</h1>"
 
+
+
+#Devolver las películas que tienen imagen de portada agregada
 @app.route("/conportada", methods=["GET"])
 def conportada():
     if modo == True:
         lista_portada = []
         for pelicula in peliculas:
-            if pelicula["imgURL"] != None:
-                lista_portada.append(pelicula["title"])
-    return lista_portada
+            if "imgURL" in pelicula:
+                lista_portada.append(pelicula["title"])        
+        return lista_portada
+    else:
+        return "<h1> No estas logueado</h1>"
 
 
 
-
-    
-       
-    
-    #Menu para usuarios ya registrados correctamente 
-    # def menu_registrado():
-    #     while (True):
-    #         system('cls')
-    #         print('Que funcion desea realizar')
-    #         print("1. Directores")
-    #         print("2. Generos")
-    #         print("3. Peliculas de un director")
-    #         print("4. Peliculas con portada")
-    #         print("5. Ver peliculas")
-    #         print("6. Eliminar pelicula")
-    #         print("7. Editar informacion de una pelicula")
-    #         print("8. Agregar una pelicula")
-    #         print("9. Salir")
-    #         print(" ")
-    #         funcion = int(input("Funcion: "))
-                
-    #         if (funcion == 1):
-    #             directores()
-                
-    #         elif (funcion == 2):
-    #             generos()
-
-    #         elif (funcion == 3):
-    #             director_peliculas()  
-
-    #         elif (funcion == 9):
-    #             break
-                
-    #         else:
-    #             print('ERROR. Opcion invalida')
-    #             input("Presiona enter para continuar")
+#Crear pelicula
+@app.route("/crear", methods=["POST"])
+def crear_pelicula():
+    if modo == True:
+        nuevapelicula = request.get_json()
+        if 'anio' and 'director' and 'genero' and 'imgURL' and 'sinopsis' and 'title' not in nuevapelicula:
+            return jsonify({"Error": "Campos erroneos o invalidos"}), HTTPStatus.BAD_REQUEST
+        nuevoid = peliculas[-1]['id'] + 1
+        titles = [t['title'] for t in peliculas]
+        if nuevapelicula["title"] not in titles:
+            pelicula = {
+            "anio": nuevapelicula["anio"],
+            "director": nuevapelicula["director"],
+            "genero": nuevapelicula["genero"],
+            "id": nuevoid,
+            "imgURL": nuevapelicula["imgURL"],
+            "sinopsis": nuevapelicula["sinopsis"],
+            "title": nuevapelicula["title"],
+            "comentarios": []
+            }
+            peliculas.append(pelicula)
+            return pelicula, HTTPStatus.OK
+        return jsonify({"Error": "Esa pelicula ya esta creada"}), HTTPStatus.BAD_REQUEST
+    else:
+        return "<h1> No estas logueado</h1>"
 
 
-    # #Verificacion de registro de usuario
-    # def registro():
-    #     while (True):
-    #         system('cls')
-    #         print("Menu de registro")
-    #         print("Escriba 'salir' para volver al menu")
-    #         usuario = input("Ingrese su nombre de usuario: ")
-    #         if usuario == "salir":
-    #             break
-    #         else:    
-    #             for nombre in usuarios:
-    #                 if usuario == nombre["usuario"]:
-    #                     id_nombre = nombre["id"]
-    #                     contraseña = input("Ingrese su contraseña: ")
-    #                     for clave in usuarios:
-    #                         if contraseña == clave["contraseña"]:
-    #                             id_contraseña = clave["id"]
-    #                             if id_nombre == id_contraseña:
-    #                                 print("Logueo correcto siguiente funcion")
-    #                                 input("presione enter para seguir ")
-    #                                 menu_registrado()
+#Crear comentario
+@app.route("/comentar/<id>", methods=["POST"])
+def comentar(id):
+    if modo == True:
+        nuevocomentario = request.get_json()
+        for pelicula in peliculas:
+            if pelicula["id"] == int(id):
+                comentarios = pelicula["comentarios"]
+                if not "comentario" in nuevocomentario:
+                    return jsonify({"Error": "se espera un comentario"}), HTTPStatus.BAD_REQUEST
+                else:
+                    comentario = {
+                        "comentario" : nuevocomentario["comentario"]
+                    }
+                    comentarios.append(comentario)
+                    return jsonify({"OK":"Se ha creado el comentario"}), HTTPStatus.OK
+        return jsonify({"Error": "No hay pelicula con ese ID"}), HTTPStatus.BAD_REQUEST
+    else:
+        return "<h1> No estas logueado</h1>"
 
-    #Menu inicial
 
-        # print('MENU DE OPCIONES')
-        # print("1. Ingresar en modo publico")
-        # print("2. Ingresar con mi cuenta")
-        # print("3. Salir")
-        # opcion = int(input("Opcion: "))
-        
-        # if (opcion == 1):
-        #     ultimas10() 
-        
-        # elif (opcion == 2):
-        #     registro()
-                
-        # elif (opcion == 3):
-        #     exit()
-            
-        # else:
-        #     print('ERROR. Opcion invalida')
-        #     input("Presiona enter para continuar")
+#Ver peliculas
+@app.route("/allpeliculas", methods=["GET"])
+def allpeliculas():
+    if modo == True:
+        todas_peliculas = []
+        for pelicula in peliculas:
+            todas_peliculas.append(pelicula)
+        return todas_peliculas
+    else:
+        return "<h1> No estas logueado</h1>"
+
+
+
+#Borrar pelicula
+@app.route("/delete/<id>", methods=["DELETE"])
+def borrarpeliculas(id):
+    if modo == True:
+        contador = 0
+        for pelicula in peliculas:
+            if pelicula["id"] == int(id):
+                if len(pelicula["comentarios"]) != 0:
+                    return jsonify({"ERROR": "No se logro borrar"}), HTTPStatus.NO_CONTENT
+                else:
+                    peliculas.pop(contador)
+                    return jsonify({"OK": "Se ha borrado la pelicula"}), HTTPStatus.OK
+            contador += 1
+    else:
+        return "<h1> No estas logueado</h1>"
+
+
+
+#Editar pelicula
+@app.route("/editar/<id>", methods=["PUT"])
+def edicion(id):
+    if modo == True:
+        nuevosdatos = request.get_json()
+        if not nuevosdatos:
+            return jsonify({"ERROR": 'No hay datos a cambiar'}), HTTPStatus.BAD_REQUEST
+        else:
+            pos = int(id)   
+            ids = [pelicula["id"] for pelicula in peliculas]
+            if pos not in ids:
+                return jsonify({"ERROR": "No hay peliculas con ese ID"}), HTTPStatus.NO_CONTENT
+            if "anio" and "director" and "genero" and "imgURL" and "sinopsis" and "title" not in nuevosdatos:
+                return jsonify({"ERROR": 'Falta informacion'}), HTTPStatus.BAD_REQUEST    
+            else:
+                for pelicula in peliculas:
+                    if pelicula["id"] == int(id):
+                        movie = peliculas[pos - 1]
+                        comm = movie["comentarios"]
+                        peliculas[pos - 1] = {
+                        "title": nuevosdatos["title"],
+                        "sinopsis": nuevosdatos["sinopsis"],
+                        "director": nuevosdatos["director"],
+                        "genero": nuevosdatos["genero"],
+                        "anio": nuevosdatos["anio"],
+                        "id": pos,
+                        "imgURL": nuevosdatos["imgURL"],
+                        "comentarios": comm
+                        }
+                        return jsonify({'mensaje':'good'}), HTTPStatus.OK
+    else:
+        return "<h1> No estas logueado</h1>"
